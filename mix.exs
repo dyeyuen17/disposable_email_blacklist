6fe3a057dbfdcdc9dc9aa8d67899e429990d769b
Jun M <jun.tomagan@secuna.io>
2020-03-10 17:18:02 +0800

    Add project description and package info

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule DisposableEmailBlacklist.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "DisposableEmailBlacklist",
+      source_url: "https://github.com/dyeyuen17/disposable_email_blacklist"
     ]
   end
 
@@ -21,9 +25,21 @@ defmodule DisposableEmailBlacklist.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.21.3"}
+      {:ex_doc, "~> 0.21.3", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp description() do
+    "Elixir library for Disposable Email Blacklist."
+  end
+
+  defp package() do
+    [
+      files: ~w(lib test .formatter.exs .gitignore README.md mix.exs mix.lock),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/dyeyuen17/disposable_email_blacklist"}
     ]
   end
 end
